2b8eb8eed8f2afc223ef2d84a5b61d1c00faaf08
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2025-11-30 19:07:09 +0530

    node_module removed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Redux Starter Project!!");
+import store from './store/configureStore'
+import { addTask, removeTask, completeTask, fetchTodos } from './store/tasks';
+
+const unsubscribe = store.subscribe(() => {
+    console.log("State updated:", store.getState());
+});
+
+store.dispatch(addTask("Task 1"))
+store.dispatch(addTask("Task 2"))
+// unsubscribe()
+store.dispatch(removeTask(1))
+store.dispatch(completeTask(2))
+store.dispatch(fetchTodos())
