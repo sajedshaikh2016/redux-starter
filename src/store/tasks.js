42d0f198cd923acd4a32834d72b24a2b5352e8cd
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2025-12-04 17:34:58 +0530

    task.js update

--- src/store/tasks.js
+++ src/store/tasks.js
@@ -1,34 +1,34 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-// Actions
-const addTask = createAction("ADD_TASK");
-const removeTask = createAction("REMOVE_TASK");
-const completeTask = createAction("TASK_COMPLETED");
-
-export { addTask, removeTask, completeTask };
-
-// Reducer
 let id = 0;
 
-export default createReducer([], (builder) => {
-    builder
-    .addCase(addTask, (state, action) => {
-        state.push({
-            id: ++id,
-            task: action.payload,
-            completed: false
-        });
-    })
-    .addCase(removeTask, (state, action) => {
-        const index = state.findIndex(task => task.id === action.payload);
-        if (index !== -1) {
-            state.splice(index, 1);
+const tasksSlice = createSlice({
+    name: 'tasks',
+    initialState: [],
+    reducers: {
+        addTask: (state, action) => {
+            state.push({
+                id: ++id,
+                task: action.payload,
+                completed: false
+            });
+        },
+        removeTask: (state, action) => {
+            const index = state.findIndex(task => task.id === action.payload);
+            if (index !== -1) {
+                state.splice(index, 1);
+            }
+        },
+        completeTask: (state, action) => {
+            const task = state.find(task => task.id === action.payload);
+            if (task) {
+                task.completed = true;
+            }
         }
-    })
-    .addCase(completeTask, (state, action) => {
-        const task = state.find(task => task.id === action.payload);
-        if (task) {
-            task.completed = true;
-        }
-    });
+    }
 })
+
+const { addTask, removeTask, completeTask } = tasksSlice.actions;
+
+export { addTask, removeTask, completeTask };
+export default tasksSlice.reducer;
